60992d22e18d58ad222c8c574cae2b847db94f07
Omid Shabab <omidshababdev@gmail.com>
2024-08-22 21:45:30 +0330

    0.1.0

--- index.ts
+++ index.ts
@@ -1,9 +1,22 @@
+import http from "http";
+import express from "express";
 import { Telegraf } from "telegraf";
 
+const port = process.env.PORT || 8080;
+
+const app = express();
+const server = http.createServer(app);
+
 const bot = new Telegraf(process.env.BOT_TOKEN!);
+
+app.use(express.json());
 
 bot.start(ctx => {
     return ctx.reply(`Hello ${ctx.update.message.from.first_name}!`);
 })
 
 bot.launch();
+
+server.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
